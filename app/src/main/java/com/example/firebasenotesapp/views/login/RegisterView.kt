b0192cdeb0ca44b37533984757671b20dd02f4cf
Jamiro <jamirocristian15@gmail.com>
2023-09-29 12:12:29 -0300

    Adding authentication.

--- app/src/main/java/com/example/firebasenotesapp/views/login/RegisterView.kt
+++ app/src/main/java/com/example/firebasenotesapp/views/login/RegisterView.kt
@@ -1,12 +1,90 @@
 package com.example.firebasenotesapp.views.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.firebasenotesapp.components.Alert
 import com.example.firebasenotesapp.viewModels.LoginViewModel
 
 
 @Composable
-fun RegisterView(navController: NavController, loginVM: LoginViewModel) {
-    Text(text = "Register View")
+fun RegisterView(navController: NavHostController, loginVM: LoginViewModel) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var username by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text(text = "UserName") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                loginVM.createUser(email, password, username) {
+                    navController.navigate("Home")
+                }
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp, end = 30.dp)
+        ) {
+            Text(text = "Register")
+        }
+        if (loginVM.showAlert) {
+            Alert(title = "ALERT",
+                message = "USER no created",
+                confirmText = "Accept",
+                onConfirmClick = { loginVM.closeAlert() }
+            ) {
+
+            }
+        }
+
+    }
 }
+
