91829874220851d15db66ea150813a04fd1edec0
Jamiro <jamirocristian15@gmail.com>
2023-09-29 11:59:10 -0300

    Creating user register 1

--- app/src/main/java/com/example/firebasenotesapp/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/example/firebasenotesapp/viewModels/LoginViewModel.kt
@@ -6,8 +6,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.firebasenotesapp.model.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -33,15 +35,16 @@ class LoginViewModel: ViewModel() {
         }
     }
 
-    fun create_user(email: String, password: String, onSuccess: () -> Unit) {
+    fun createUser(email: String, password: String, username: String, onSuccess: () -> Unit) {
         viewModelScope.launch {
             try {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener {task ->
                         if (task.isSuccessful) {
+                            saveUser(username)
                             onSuccess()
                         }else {
-                            Log.d("Firebase Error", "User and password are wrong")
+                            Log.d("Firebase Error", "Error creating user")
                             showAlert = true
                         }
                     }
@@ -50,6 +53,24 @@ class LoginViewModel: ViewModel() {
             }
         }
     }
+    private fun saveUser(userName: String) {
+        val id = auth.currentUser?.uid
+        val email = auth.currentUser?.email
+
+        val user = UserModel(
+            userId = id.toString(),
+            email = email.toString(),
+            userName = userName
+        )
+        FirebaseFirestore.getInstance().collection("Users")
+            .add(user)
+            .addOnSuccessListener {
+                Log.d("Data saved", "Successfully saving data")
+            }.addOnFailureListener{
+                Log.d("Error saving", "Error saving in firestore")
+            }
+    }
+
     fun closeAlert() {
         showAlert = false
     }
